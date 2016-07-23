c5f7ab196dfd20b1c2f412afff48baa524943fc0
jy <jy@163.com>
2016-07-23 15:54:56 +0800

    Intent-

--- app/src/main/java/com/example/lsx/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/lsx/activitylifecycle/MainActivity.java
@@ -13,23 +13,34 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     public static final String BOOK_NAME_OF_CATEGORY_PHYSICAL="bookNameOfCategoryphysical";
     private Button mButton;
+    private EditText mEditText;
+    private Button mThirdButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreat:");
         mButton=(Button) findViewById(R.id.activity_main_button);
-        final EditText  editText=(EditText) findViewById(R.id.activity_main_edit_text);
+        mEditText=(EditText) findViewById(R.id.activity_main_edit_text);
 
         mButton.setOnClickListener(new View.OnClickListener() {
          @Override
         public void onClick(View v) {
         Log.d(TAG,"onClick:");
         Intent intent=new Intent(MainActivity.this,ActivitySecond.class);
-        intent.putExtra(BOOK_NAME_OF_CATEGORY_PHYSICAL,editText.getText().toString());
+        intent.putExtra(BOOK_NAME_OF_CATEGORY_PHYSICAL,mEditText.getText().toString());
         startActivityForResult(intent,0);
     }
 });
+        mThirdButton=(Button) findViewById(R.id.activity_main_third_button);
+        mThirdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ActivityThird.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
